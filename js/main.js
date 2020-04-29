5b8e2e421e8d61d4732d4c086f541a622951697d
Carmine Lentisco <carminelentisco@gmail.com>
2020-04-29 16:40:24 +0200

    JS
    Correzione del codice

--- js/main.js
+++ js/main.js
@@ -1,120 +1,70 @@
-/**
- * WELCOME TO MOMENT JS
- */
+
 $(document).ready(function () {
-    
-    // Setup
 
-    // Punto di partenza
-    var baseMonth = moment('2018-01-01'); 
-
-    // Init Hndlenars
-    var source = $('#day-template').html();
+    var baseDate= moment('2018-01-01');                 // DATA INIZIALE
+    var dateClone = moment(baseDate);                   // CLONE DATA INIZIALE
+    var btnIndietro = $('.btn-indietro');               // PULSANTE INDIETRO
+    var btnAvanti = $('.btn-avanti');                   // PULSANTE AVANTI
+    var source = $('#day-template').html();             // INIT HANDLEBARS
     var template = Handlebars.compile(source);
-
-    // print giorno
-    printMonth(template, baseMonth);
-
-    // ottieni festività mese corrente
-    printHoliday(baseMonth);
-
-    /**
-     * 
-     * Seconda parte
-     *  
-    */
-
-    /******************************************************************* */
-
-    // Clone della data originale
-    var dateClone = moment(baseMonth);  // Copia della data
-     
-
-    // numero giorni nel mese
     
-    
-    // Collegamento ai bottoni
-    var btnIndietro = $('.btn-indietro');
-    var btnAvanti = $('.btn-avanti');
-    
- 
+    printMonth(template, baseDate);
+    printHoliday(baseDate);
+
     btnAvanti.on('click', function() {
-    
-        // Manda avanti i mesi di 1
         var testdate = dateClone;
 
-        console.log(testdate.month());
-        
         if ( testdate.month() < 11 ) {
             dateClone.add(1, 'M');
             printMonth(template,dateClone);
             printHoliday(dateClone);
-        }
-                
+        } 
     });
 
     btnIndietro.on('click', function() {
-    
-        // Manda avanti i mesi di 1
-        dateClone.subtract(1, 'M');
-        $('.month-list').html(' ');
-        printMonth(template,dateClone);
-        printHoliday(dateClone);
-
-        if ( dateClone.month() === 0 ) {
-            alert('Go forword!!');
-        }
-                
+        var testdate = dateClone;
+        
+        if ( testdate.month() > 0 ) {
+            dateClone.subtract(1, 'M');
+            printMonth(template,dateClone);
+            printHoliday(dateClone);
+        }        
     });
     
 
 }); // <-- End doc ready
 
 
-/*************************************
-    FUNCTIONS
- *************************************/
+/************************************* FUNCTIONS *************************************/
 
 // Stampa a schermo i giorni del mese
 function printMonth(template, date ) {
-    $('.month-list').html(' ');
-    
-    
-    //  setta header
-    $('h1').html( date.format('MMMM YYYY') );
+    $('.month-list').html(' ');                                                 // PULIZIA DEL MARCKUP PRECEDENTE
+    $('h1').html( date.format('MMMM YYYY') );                                   // CREAZIONE HEADER 
+    $('.month').attr('data-this-date',  date.format('YYYY-MM-DD'));             // IMPOSTA IL DATA ATTRIBUTE DELLA DATA VISUALIZZATA
 
-    // Imposta data attribute data visualizzata
-    $('.month').attr('data-this-date',  date.format('YYYY-MM-DD'));
-    
     var daysInMonth = date.daysInMonth();
-    // genera giorni mese
+
     for (var i = 0; i < daysInMonth; i++) {
-        // genera data con moment js
         var thisDate = moment({
             year: date.year(),
             month: date.month(),
             day: i + 1
         });
-
-        // imposta dati template
-        var context = {
+        var context = {                                                         // OGGETTO PER IL TEPLATE
             class: 'day',
             day: thisDate.format('DD MMMM'),
             completeDate: thisDate.format('YYYY-MM-DD')
-        };
-
-        //compilare e aggiungere template
-        var html = template(context);
+        };   
+        var html = template(context);                                           
         $('.month-list').append(html);
-
     }
         
-
 }
 
 // Ottieni e stampa festività
 function printHoliday(date) {
-    // chiamo API
+
     $.ajax({
         url: 'https://flynn.boolean.careers/exercises/api/holidays' ,
         method: 'GET',
@@ -126,8 +76,8 @@ function printHoliday(date) {
             var holidays = res.response;
 
             for (var i = 0; i < holidays.length; i++) {
+                
                 var thisHoliday = holidays[i];
-
                 var listItem = $('li[data-complete-date="' + thisHoliday.date + '"]');
 
                 if(listItem) {
